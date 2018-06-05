ace46cafb24db47b8de35d777220e4974dc95a00
Ewa-K <ewa-1988@yahoo.com>
2018-06-05 14:11:41 +0200

    Change app

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 var movies = [{
-		id: 1,
+        id: 1,
         title: 'Harry Potter',
-        desc: 'Film o czarodzieju.'
+        desc: 'Film o czarodzieju.',
+        foto: 'harry.jpg'
     },
     {
-    	id: 2,
+        id: 2,
         title: 'Król Lew',
-        desc: 'Film o królu sawanny.'
+        desc: 'Film o królu sawanny.',
+        foto: 'lew.jpeg'
     },
     {
-    	id: 3,
-    	title: 'Dzwonnik z Notre Dame.',
-    	desc: 'Film o Quasimodo.'
+        id: 3,
+        title: 'Dzwonnik z Notre Dame.',
+        desc: 'Film o Quasimodo.',
+        foto: 'dzwonnik.jpeg'
     },
     {
-    	id: 4,
-    	title: 'Księżniczka i żaba',
-    	desc: 'Film o  o dziewczynie z Nowego Orleanu i księciu zamienionym w żabę, desperacko pragnącym być znów człowiekiem.'
+        id: 4,
+        title: 'Księżniczka i żaba',
+        desc: 'Film o  o dziewczynie z Nowego Orleanu i księciu zamienionym w żabę, desperacko pragnącym być znów człowiekiem.',
+        foto: 'ksiezniczka.png'
     }
 ];
 
 var moviesElements = movies.map(function(movie) {
-    return React.createElement('li', {key:movie.id},
+    return React.createElement('li', { key: movie.id },
         React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc)
-            );
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {}, movie.foto)
+    );
 });
 
 var element =
